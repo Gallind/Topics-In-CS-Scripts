efec54755e421cd40003be76d53a0a311b3b1c12
gal.lind <gal.lind@post.runi.ac.il>
2025-12-05 20:28:12 +0200

    Added the new folder and implemented first helper function

---
+++ hw6/MillerRabin.java
@@ -0,0 +1,22 @@
+package hw6;
+
+import java.math.BigInteger;
+
+import CommonFunctions.*;
+
+public class MillerRabin {
+
+    /*
+    Make sure that N is odd
+    */
+    private static DuoTuple compute_UR(BigInteger N){
+        if (N.mod(BigInteger.TWO) == BigInteger.ZERO) return new DuoTuple();
+        N  = N.subtract(BigInteger.ONE);
+        BigInteger r = BigInteger.ZERO;
+        while (N.mod(BigInteger.TWO) == BigInteger.ZERO){
+            r = r.shiftLeft(1);
+            N = N.shiftRight(1);
+        }
+        return new DuoTuple(N, r);
+    }
+}
