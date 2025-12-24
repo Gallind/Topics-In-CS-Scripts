801e48f641984f514105789de045d2a8d8261e97
gal.lind <gal.lind@post.runi.ac.il>
2025-12-24 14:44:29 +0200

    Created the main class for this hw and implemented simple helper methods

---
+++ hw7/DHProtocol.java
@@ -0,0 +1,39 @@
+package hw7;
+
+import java.math.BigInteger;
+
+import CommonFunctions.BigRandom;
+import CommonFunctions.DuoTuple;
+import hw6.MillerRabin;
+import hw7.DHGroupSetup.*;
+
+public class DHProtocol {
+    /**
+     * Choosing randomly a number x in the group of order q and computing g^x mod p 
+     * @param p the order of the big group so we do mod p
+     * @param q the order of the subgroup of quadratic residues G
+     * @param g the generator of G
+     * @return (x, exp) where @param x is the random element from G and @param exp is g^x mod p
+     */
+    public static DuoTuple chooseKeyParts(BigInteger p, BigInteger q, BigInteger g){
+        //Sampling a random x from [2, q-1] because 1 is boring
+        BigInteger x = BigRandom.randomRange(q.subtract(BigInteger.TWO)).add(BigInteger.TWO);
+        BigInteger exp = MillerRabin.modExp(g, x, p);
+        return new DuoTuple(x, exp);
+    }
+    /**
+     * Computing the shared key of alice and bob. computing (g^a)^b mod p
+     * @param exp = g^a for a generator g and a sampled number a
+     * @param x the exponent b
+     * @param p the order of the group
+     * @return the shared key that is (g^a)^b mod p
+     */
+    public static BigInteger computeSharedKey(BigInteger exp, BigInteger x, BigInteger p){
+        BigInteger sharedKey = MillerRabin.modExp(exp, x, p);
+        return sharedKey;
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
